533b90d05fe057cb451398c8518c31875e3682d3
Nguyen The Thuc <thucfami@gmail.com>
2015-11-06 22:40:55 +0700

    Quyen sua bai viet permission_monitor

--- views/matran/huongdan.php
+++ views/matran/huongdan.php
@@ -7,7 +7,7 @@
             ?>
             <strong><p><?=$model->title?></p></strong>
             <div ><?=$model->description?></div>
-            <?php if(Yii::$app->user->can('permission_admin')){?>
+            <?php if(Yii::$app->user->can('permission_monitor')){?>
             <a href="/book/update.html?id=<?=$model->id?>" title="Update" aria-label="Update" data-pjax="0">
                 <span class="glyphicon glyphicon-pencil"></span>
             </a>
